b3b6f929c26ea28bbe20630bfa7943acc01698ef
tqndev <uznaruto298@gmail.com>
2026-03-20 16:24:24 +0700

    update

--- src/api.py
+++ src/api.py
@@ -4,6 +4,6 @@ from src.auth.controller import router as auth_router
 from src.users.controller import router as users_router
 
 def register_routes(app: FastAPI):
-    app.include_router(todos_router)
     app.include_router(auth_router)
+    app.include_router(todos_router)
     app.include_router(users_router)
